979790324cabe4ed1e9041e015908be91d95fb6f
JoaoHLira <lirarosas.jh@gmail.com>
2026-02-16 13:15:25 -0300

    first commit

---
+++ src/test/java/br/com/joaolirarosas/cupon_outforce/CuponOutforceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaolirarosas.cupon_outforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuponOutforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
