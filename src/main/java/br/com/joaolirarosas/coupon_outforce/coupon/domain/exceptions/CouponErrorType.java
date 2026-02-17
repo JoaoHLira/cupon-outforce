99fd74cc84eb680103024b57b98d77f8a3f7b1db
JoaoHLira <lirarosas.jh@gmail.com>
2026-02-17 20:29:17 -0300

    refactor: muda enum de CODE_STATUS para COUPON_STATUS

--- src/main/java/br/com/joaolirarosas/coupon_outforce/coupon/domain/exceptions/CouponErrorType.java
+++ src/main/java/br/com/joaolirarosas/coupon_outforce/coupon/domain/exceptions/CouponErrorType.java
@@ -1,5 +1,8 @@
 package br.com.joaolirarosas.coupon_outforce.coupon.domain.exceptions;
 
 public enum CouponErrorType {
-    CODE, EXPIRATION_DATE, CODE_STATUS, DISCOUNT
+    CODE,
+    EXPIRATION_DATE,
+    COUPON_STATUS,
+    DISCOUNT
 }
