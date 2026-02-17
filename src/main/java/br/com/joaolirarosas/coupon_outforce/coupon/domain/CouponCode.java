a9eceedb86035393a3d9e321733c73af7839c6f0
JoaoHLira <lirarosas.jh@gmail.com>
2026-02-17 10:03:11 -0300

    feat: estrutura dominio Coupon e seus objetos de valor com regras de negocio e validacoes para criacao e mudanca de estado

---
+++ src/main/java/br/com/joaolirarosas/coupon_outforce/coupon/domain/CouponCode.java
@@ -0,0 +1,45 @@
+package br.com.joaolirarosas.coupon_outforce.coupon.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CouponCode {
+
+    @Column(name = "code", length = 6, nullable = false)
+    private String code;
+
+    public CouponCode(String code) {
+        String codeFormated = removeEspecialCharacters(code);
+        validateCode(codeFormated);
+        this.code = codeFormated;
+    }
+
+    private void validateCode(String code) {
+        checkIfCodeIsBlanckOrNull(code);
+        checkSizeOfTheCode(code);
+    }
+
+    private static final Integer CODE_SIZE = 6;
+
+    private void checkIfCodeIsBlanckOrNull(String code) {
+        if (code.isBlank()) {
+            throw new RuntimeException("O código não deve estar vazio");
+        }
+    }
+
+    private void checkSizeOfTheCode(String code) {
+        if (code.length() != CODE_SIZE) {
+            throw new RuntimeException("O código deve ter o seu tamanho igual a " + CODE_SIZE);
+        }
+    }
+
+    private String removeEspecialCharacters(String code) {
+        return code.replaceAll("[^a-zA-Z0-9]", "");
+    }
+}
